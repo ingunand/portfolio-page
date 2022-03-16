879dc66c291e4af59a5e24557ceea0ac7e778814
Ingunn Hatlehol Andreassen <54577850+ingunand@users.noreply.github.com>
2022-03-16 23:10:13 +0100

    basic setup and components

---
+++ portfolio/src/components/aboutme.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/aboutme.css';
+
+class AboutMe extends React.Component {
+
+    render() {
+      return (
+            <div id="aboutMeMain">
+                
+            </div>
+            );
+    }
+}
+
+export default AboutMe;
---
+++ portfolio/src/components/projectsSummary.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/projectsSummary.css';
+
+class ProjectsSummary extends React.Component {
+
+    render() {
+      return (
+            <div id="projectsSummaryMain">
+                
+            </div>
+            );
+    }
+}
+
+export default ProjectsSummary;
