3b67c27d19384d086121061e622e768ecf32a4bc
Ingunn <54577850+ingunand@users.noreply.github.com>
2023-01-16 17:25:11 +0100

    Update aboutme.jsx

--- src/components/aboutme.jsx
+++ src/components/aboutme.jsx
@@ -12,7 +12,7 @@ class AboutMe extends React.Component {
             <div id="aboutMeMain" className="frontpageParts">
               <div id="aboutMeContent">
                 <h2>About me</h2>
-                <p>I am soon done with a bachelor's degree in web development from NTNU in Gjøvik, Norway. 
+                <p>I have a bachelor's degree in web development from NTNU in Gjøvik, Norway. 
                   I am most passionate about front-end and CSS, and I love to watch things take form on my screen.</p>
                 <p>I am originaly from Vatne, Møre og Romsdal, but I moved to Gjøvik to study in 2019. 
                   I have a love for lemurs and dogs, I love watermelon and I eat tacos every week!</p>
@@ -24,7 +24,7 @@ class AboutMe extends React.Component {
                 <p>Take a look at my projects, and feel free to contact me!</p>
 
                   <a href="https://www.linkedin.com/in/ingunn-hatlehol-andreassen/"><img className="logo" src={linkedin} alt="linkedin logo"/></a>
-                  <a href="mailto:ingunand@stud.ntnu.no"><img className="logo" alt="mail envelope" src={email}/></a>
+                  <a href="mailto:ingunnhatlehol@hotmail.com"><img className="logo" alt="mail envelope" src={email}/></a>
                   <a href="https://github.com/ingunand"><img className="logo" alt="github logo" src={github}/></a>
               </div>
             </div>
